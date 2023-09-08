c4752861f22e015dc87e136f8e2ffb6660abdfe0
Andrzej Szlachta <andrzej.sz123@gmail.com>
2023-09-08 14:10:22 +0200

    Add content and routing

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,9 +1,7 @@
-function PageNotFound() {
+export default function PageNotFound() {
   return (
     <div>
-      Not found
+      <h1>Page not found 😢</h1>
     </div>
-  )
+  );
 }
-
-export default PageNotFound
