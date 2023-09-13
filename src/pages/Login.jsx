7b7cc32a17172c264439766e8e13b5017733f34e
Andrzej Szlachta <andrzej.sz123@gmail.com>
2023-09-14 00:05:26 +0200

    Add fake authorization

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,25 @@
 import styles from "./Login.module.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useAuth } from '../contexts/FakeAuthContext';
+import { useNavigate } from "react-router-dom";
+import Button from "../components/Button";
 
 export default function Login() {
+  const navigate = useNavigate()
+  const { login, isAuthenticated } = useAuth()
   // PRE-FILL FOR DEV PURPOSES
   const [email, setEmail] = useState("jack@example.com");
   const [password, setPassword] = useState("qwerty");
+
+  useEffect(function() {
+    if (isAuthenticated === true) navigate("/app", { replace: true })
+  }, [isAuthenticated, navigate])
+
+  function handleLogin(e) {
+    e.preventDefault()
+    if (!email || !password) return
+    login(email, password)
+  }
 
   return (
     <main className={styles.login}>
@@ -30,7 +45,7 @@ export default function Login() {
         </div>
 
         <div>
-          <button>Login</button>
+          <Button type="primary" onClick={handleLogin}>Login</Button>
         </div>
       </form>
     </main>
